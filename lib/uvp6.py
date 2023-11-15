ac1f5c73f3a612ae407cd10301e43c1df7b4caaa
Jake Bonney <jakebonney10@uri.edu>
2023-11-15 12:36:27 -0500

    Added functions for parsing the different serial messages.

--- lib/uvp6.py
+++ lib/uvp6.py
@@ -27,4 +27,42 @@ class UVP6:
     def autocheck(self):
         return self.send_command('$autocheck;')
 
-    # Additional functions to parse HWconf, ACQconf, LPM_DATA, BLACK_DATA etc.
+    def parse_hwconf(self, hwconf_frame):
+        hwconf_data = hwconf_frame.split(',')
+        # Assuming hwconf_data follows the known format, map it to a dictionary
+        hwconf_dict = {
+            "HW_CONF": hwconf_data[0],
+            "Camera_ref": hwconf_data[1],
+            # Continue mapping all the known parameters
+        }
+        return hwconf_dict
+
+    def parse_acqconf(self, acqconf_frame):
+        acqconf_data = acqconf_frame.split(',')
+        acqconf_dict = {
+            "ACQ_CONF": acqconf_data[0],
+            "Configuration_name": acqconf_data[1],
+            # Continue mapping all the known parameters
+        }
+        return acqconf_dict
+
+    def parse_lpm_data(self, lpm_data_frame):
+        lpm_data = lpm_data_frame.split(',')
+        lpm_data_dict = {
+            "LPM_DATA": lpm_data[0],
+            "Depth": float(lpm_data[1]),
+            # Continue mapping all the known parameters
+        }
+        return lpm_data_dict
+
+    def parse_black_data(self, black_data_frame):
+        black_data = black_data_frame.split(',')
+        black_data_dict = {
+            "BLACK_DATA": black_data[0],
+            "Depth": float(black_data[1]),
+            # Continue mapping all the known parameters
+        }
+        return black_data_dict
+
+    # Add any additional methods needed for handling responses
+
