a4aba48324ea63af2508d6ec5286af9dae99293f
Jake Bonney <jakebonney10@uri.edu>
2023-11-28 15:48:27 -0500

    Changes to UVP6 node to reflect library changes.
    Basic setup of node, this will not work yet. Need to change the main loop and incorporate instrument_check function.

--- scripts/uvp6_driver.py
+++ scripts/uvp6_driver.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python
 import rospy
-from uvp6.msg import HWconfMsg, ACQconfMsg, LPMDataMsg, BlackDataMsg
-from uvp6 import UVP6  # Make sure uvp6.py is in the same package or properly installed
+from uvp6.msg import HwConf, AcqConf, LpmData, BlackData, TaxoConf, TaxoData
+from uvp6 import UVP6
 
 class UVP6DriverNode:
     def __init__(self):
-        # Initialize the node
+        # Initialize the ROS node
         rospy.init_node('uvp6_driver')
 
         # Retrieve serial connection cfg from the parameter server or use default values
@@ -13,20 +13,35 @@ class UVP6DriverNode:
         baudrate = rospy.get_param('~baudrate', 9600)
 
         # Create a UVP6 object
-        self.uvp6 = UVP6(port, baudrate)  # Adjust the port and baud rate as needed
+        self.uvp6 = UVP6(port, baudrate)  # Initialize the serial connection
 
         # Publishers for each message type
-        self.hwconf_pub = rospy.Publisher('hwconf', HWconfMsg, queue_size=10)
-        self.acqconf_pub = rospy.Publisher('acqconf', ACQconfMsg, queue_size=10)
-        self.lpmdata_pub = rospy.Publisher('lpm_data', LPMDataMsg, queue_size=10)
-        self.blackdata_pub = rospy.Publisher('black_data', BlackDataMsg, queue_size=10)
+        self.hwconf_pub = rospy.Publisher('hw_conf', HwConf, queue_size=10)
+        self.acqconf_pub = rospy.Publisher('acq_conf', AcqConf, queue_size=10)
+        self.taxoconf_pub = rospy.Publisher('taxo_conf', TaxoConf, queue_size=10)
+        self.lpmdata_pub = rospy.Publisher('lpm_data', LpmData, queue_size=10)
+        self.blackdata_pub = rospy.Publisher('black_data', BlackData, queue_size=10)
+        self.taxodata_pub = rospy.Publisher('black_data', TaxoData, queue_size=10)
 
-    def publish_data(self, msg_type, data):
+
+    def publish_data(self, msg_type, parsed_data):
+        # Publish data based on the message type
         if msg_type == "HW_CONF":
-            self.hwconf_pub.publish(data)
+            msg = HwConf()  # Create and populate the HWconfMsg
+            # Populate msg fields with parsed_data
+            self.hwconf_pub.publish(msg)
         elif msg_type == "ACQ_CONF":
-            self.acqconf_pub.publish(data)
-        # Add similar conditions for LPM_DATA and BLACK_DATA
+            msg = AcqConf()  # Create and populate the ACQconfMsg
+            # Populate msg fields with parsed_data
+            self.acqconf_pub.publish(msg)
+        elif msg_type == "LPM_DATA":
+            msg = LpmData()  # Create and populate the LPMDataMsg
+            # Populate msg fields with parsed_data
+            self.lpmdata_pub.publish(msg)
+        elif msg_type == "BLACK_DATA":
+            msg = BlackData()  # Create and populate the BlackDataMsg
+            # Populate msg fields with parsed_data
+            self.blackdata_pub.publish(msg)
 
     def run(self):
         # Start data acquisition - adjust the parameters as needed
@@ -36,10 +51,11 @@ class UVP6DriverNode:
         while not rospy.is_shutdown():
             # Read serial data, parse it and publish
             try:
-                serial_data = self.uvp6.ser.readline().decode().strip()
+                serial_data = self.uvp6.read_response()
                 if serial_data:
                     msg_type, parsed_data = self.uvp6.parse_message(serial_data)
-                    self.publish_data(msg_type, parsed_data)
+                    if parsed_data:
+                        self.publish_data(msg_type, parsed_data)
             except Exception as e:
                 rospy.logerr("Error reading from UVP6: %s", e)
                 break
